77b000bbc5f8f699044151dfd773b8722e0d7652
Junfang Jiang <junfang@berkeley.edu>
2018-06-19 21:59:28 +0800

    Update deploy script to support python3 + update model regression fc (add multiple layer with relu activation)

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,14 +1,51 @@
 import argparse
 import os
 import subprocess
-import thread
+import threading
 import time
-import urlparse
+try:
+    from urllib.parse import urlparse, parse_qs
+except ImportError:
+    from urlparse import urlparse, parse_qs
 
 from utils import *
 
-parser = argparse.ArgumentParser(description='PAI deployment')
-parser.add_argument('odps_path', help='Path to odps console')
+# Path to store code.tar.gz
+tar_ball_location = os.path.join(os.path.dirname(os.path.abspath(__file__)), "code.tar.gz")
+
+# Main entry file of the code
+main_entry_file = "main.py"
+
+# Pai commands
+pai_commands = [
+    "use kelude_open_dw;",
+    "pai -name tensorflow140"
+    + " -Dscript='file://{}'".format(tar_ball_location)
+    + " -DentryFile='{}'".format(main_entry_file)
+    + " -DcheckpointDir='oss://apsalgo-hz/?role_arn=acs:ram::1396633922344963:role/aps-odps-algo&host=cn-hangzhou.oss-internal.aliyun-inc.com'"
+    + " -Dbuckets='oss://apsalgo-hz/?role_arn=acs:ram::1396633922344963:role/aps-odps-algo&host=cn-hangzhou.oss-internal.aliyun-inc.com';"
+]
+
+parser = argparse.ArgumentParser(description="""
+A script that helps you to automatically upload PAI tasks.
+
+* Put the entire deploy folder WITHIN your project directory. The script can be used as
+  > python deploy.py ABS_PATH_TO_ODPSCMD [--suppress_stderr]
+
+* Your project should be managed under git, and only files tracked by git will be uploaded to PAI.
+  To see which files will be uploaded, run the following command
+  > git ls-tree --full-tree -r --name-only HEAD
+
+* Currently, the entry file is configured to be <YOUR_PROJECT_ROOT>/main.py. If your entry file is
+  not this file, please open deploy.py and change variable 'main_entry_file'.
+
+* To add hyperparameter or any other pai command arguments, please open deploy.py and change variable
+  'pai_commands'.
+
+* You may use optional flag --suppress_stderr to hide messages from stderr.
+
+""", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('path_to_odpscmd', help='Path to odps console')
 parser.add_argument('--suppress_stderr', action='store_true')
 args = parser.parse_args()
 
@@ -17,12 +54,6 @@ odps_path = args.odps_path
 
 # boolean: if show stderr message
 suppress_stderr = args.suppress_stderr
-
-# Path to store code.tar.gz
-tar_ball_location = os.path.join(os.path.dirname(os.path.abspath(__file__)), "code.tar.gz")
-
-# Main entry file of the code
-main_entry_file = "main.py"
 
 # Job url returned from odps
 job_url = None
@@ -53,15 +84,6 @@ process = subprocess.Popen([odps_path],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
 
-commands = [
-    "use kelude_open_dw;",
-    "pai -name tensorflow140"
-    + " -Dscript='file://{}'".format(tar_ball_location)
-    + " -DentryFile='{}'".format(main_entry_file)
-    + " -DcheckpointDir='oss://apsalgo-hz/?role_arn=acs:ram::1396633922344963:role/aps-odps-algo&host=cn-hangzhou.oss-internal.aliyun-inc.com'"
-    + " -Dbuckets='oss://apsalgo-hz/?role_arn=acs:ram::1396633922344963:role/aps-odps-algo&host=cn-hangzhou.oss-internal.aliyun-inc.com';"
-]
-
 
 def output_handler(buf):
     global job_url
@@ -73,16 +95,18 @@ def output_handler(buf):
             process.terminate()
     if out.startswith('odps@') and out.endswith('>'):
         del buf[:]
-        if len(commands) > 0:
-            process.stdin.write(commands[0] + "\n")
-            commands.pop(0)
+        if len(pai_commands) > 0:
+            process.stdin.write((pai_commands[0] + "\n").encode())
+            pai_commands.pop(0)
             process.stdin.flush()
         else:
             process.terminate()
 
 
-thread.start_new_thread(forward_fd, (process.stderr, sys.stdout, output_handler, lambda: job_url is not None))
-thread.start_new_thread(forward_fd, (process.stdout, sys.stdout, output_handler, lambda: job_url is not None))
+threading.Thread(target=forward_fd,
+                 args=(process.stderr, sys.stdout, output_handler, lambda: job_url is not None)).start()
+threading.Thread(target=forward_fd,
+                 args=(process.stdout, sys.stdout, output_handler, lambda: job_url is not None)).start()
 
 process.wait()
 
@@ -91,9 +115,9 @@ print("Pai job launched. Retrieving job information...\n")
 ###################################
 #  Retrieve Job ID and Status
 ###################################
-parsed = urlparse.urlparse(job_url)
-job_id = urlparse.parse_qs(parsed.query)['i'][0].strip()
-token = urlparse.parse_qs(parsed.query)['token'][0].strip()
+parsed = urlparse(job_url)
+job_id = parse_qs(parsed.query)['i'][0].strip()
+token = parse_qs(parsed.query)['token'][0].strip()
 
 print("Job ID: {}\nToken: {}\n".format(job_id, token))
 
--- model.py
+++ model.py
@@ -35,7 +35,7 @@ class Code2VecModel:
         embedding_path = tf.nn.embedding_lookup(path_embedding, path)
         embedding_end = tf.nn.embedding_lookup(node_embedding, end)
         inputs = tf.concat([embedding_start, embedding_path, embedding_end], axis=2)
-        tf.logging.info("Code2VecModel - Embedding output:  %s" % inputs)
+        tf.logging.info("Code2VecModel - Embedding Layer:  %s" % inputs)
         return inputs
 
     # FC1
@@ -49,10 +49,15 @@ class Code2VecModel:
                                              initializer=tf.contrib.layers.xavier_initializer(),
                                              dtype=tf.float32)
             flatten_input = tf.reshape(inputs, [-1, context_size])
-            encode_dropout = tf.layers.dropout(flatten_input, opt.dropout_rate)
+
+            encode_dropout = tf.layers.dropout(flatten_input,
+                                               rate=opt.dropout_rate,
+                                               training=opt.training)
+
             flatten_encode_inputs = tf.nn.tanh(tf.matmul(encode_dropout, encode_weights))
+
             encode_inputs = tf.reshape(flatten_encode_inputs, [-1, bag_size, opt.encode_size])
-            tf.logging.info('Code2VecModel - Encoded output:    %s' % encode_inputs)
+            tf.logging.info('Code2VecModel - Encoding Layer:   %s' % encode_inputs)
             return encode_inputs
 
     # Attention layer
@@ -62,29 +67,54 @@ class Code2VecModel:
             bag_size = inputs.get_shape()[1]
             encode_size = inputs.get_shape()[2]
             context_weights = tf.contrib.layers.fully_connected(inputs, 1, activation_fn=None)
+
             context_weights_masked = tf.where(tf.reshape(mask, [-1, bag_size, 1]), context_weights,
                                               tf.multiply(tf.ones_like(context_weights), -3.4e38))
+
             context_weights_softmax = tf.nn.softmax(context_weights_masked, 1)
+
             context_weights_repeat = tf.tile(context_weights_softmax, [1, 1, encode_size])
             context_mul = tf.multiply(inputs, context_weights_repeat)
             context_sum = tf.reduce_sum(context_mul, 1)
-            tf.logging.info("Code2VecModel - Attention output:  %s" % context_sum)
+            tf.logging.info("Code2VecModel - Attention Layer:  %s" % context_sum)
             return context_sum
 
     # Regression
     @staticmethod
     def build_regression(inputs, score, opt):
         with tf.name_scope("regression"):
-            batch_size = inputs.get_shape()[0]
             bag_size = inputs.get_shape()[1]
-            output_weights = tf.get_variable("output_weights",
-                                             [bag_size, 1],
-                                             initializer=tf.contrib.layers.xavier_initializer(),
-                                             dtype=tf.float32)
-            dropout_inputs = tf.layers.dropout(inputs,
-                                               rate=opt.dropout_rate,
-                                               training=opt.training)
-            outputs = tf.matmul(dropout_inputs, output_weights)
+
+            dropout_inputs_1 = tf.layers.dropout(inputs,
+                                                 rate=opt.dropout_rate,
+                                                 training=opt.training)
+
+            w_1 = tf.get_variable("regression_w1",
+                                  [bag_size, bag_size],
+                                  initializer=tf.contrib.layers.xavier_initializer(),
+                                  dtype=tf.float32)
+            b_1 = tf.get_variable("regression_b1",
+                                  [bag_size],
+                                  initializer=tf.zeros_initializer(),
+                                  dtype=tf.float32)
+
+            outputs_1 = tf.nn.relu(tf.matmul(dropout_inputs_1, w_1) + b_1)
+
+            dropout_inputs_2 = tf.layers.dropout(outputs_1,
+                                                 rate=opt.dropout_rate,
+                                                 training=opt.training)
+
+            w_2 = tf.get_variable("regression_w2",
+                                  [bag_size, 1],
+                                  initializer=tf.contrib.layers.xavier_initializer(),
+                                  dtype=tf.float32)
+            b_2 = tf.get_variable("regression_b2",
+                                  [1],
+                                  initializer=tf.zeros_initializer(),
+                                  dtype=tf.float32)
+
+            outputs = tf.matmul(dropout_inputs_2, w_2) + b_2
+
             loss = tf.losses.mean_squared_error(score, tf.reshape(outputs, [-1]))
-            tf.logging.info("Code2VecModel - Regression output: %s" % outputs)
+            tf.logging.info("Code2VecModel - Regression Layer: %s" % outputs)
             return loss, outputs
