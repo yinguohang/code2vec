3e7cac7c9b994c57ae28ca54ffff5c781c86df3d
Junfang Jiang <junfang@berkeley.edu>
2018-07-02 12:08:37 +0800

    Optimal concat [mse=0.0668]

--- utils/config.py
+++ utils/config.py
@@ -37,7 +37,7 @@ def init():
     flags.DEFINE_string("optimizer", "adam", "Selected optimizer")
 
     if flags.FLAGS.optimizer == "adam":
-        flags.DEFINE_string("learning_rate", 0.0005, "Learning rate")
+        flags.DEFINE_string("learning_rate", 0.0002, "Learning rate")
     elif flags.FLAGS.optimizer == "adadelta":
         flags.DEFINE_string("learning_rate", 1.0, "Learning rate")
 
@@ -61,7 +61,7 @@ def init():
     flags.DEFINE_integer("embedding_node_size", 100,
                          "Node (start and end) embedding size")
 
-    flags.DEFINE_integer("embedding_path_size", 250,
+    flags.DEFINE_integer("embedding_path_size", 350,
                          "Path embedding size")
 
     ############################
@@ -69,7 +69,7 @@ def init():
     ############################
 
     # Encoding Layer Structure Parameter
-    flags.DEFINE_integer("encoding_size", 150, "Context encoding size")
+    flags.DEFINE_integer("encoding_size", 250, "Context encoding size")
 
     # Encoding Layer Penalty Parameter
     flags.DEFINE_float("encoding_weight_penalty_rate", 0.03, "Encoding layer penalty rate")
